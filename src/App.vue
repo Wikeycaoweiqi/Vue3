9ccf26f8dbef9f802fb9ac7dd88398ca16b4e003
Wikey Cao <wikeycaoweiqi@163.com>
2024-03-30 02:58:46 +0800

    vue3 initiation

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="app">
+    <h1>Hello Vue3</h1>
+  </div>
+</template>
+
+<script lang='js'>
+export default {
+  name: "App"
+}
+</script>
+
+<style scoped>
+.app {
+  background-color: #ddd;
+  box-shadow: 0 0 10px;
+  border-radius: 10px;
+  padding: 20px;
+}
+</style>
